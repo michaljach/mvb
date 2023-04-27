4877703b39c2ae4fb979ae19c9b1d59ccf4580e7
Michael Jach <michaljach@gmail.com>
2023-04-27 21:01:37 +0200

    github workflow

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPassthroughCopy("style.css");
 
   return {
     dir: {
       output: "docs",
-      pathPrefix: "/mvb/",
     },
   };
 };
